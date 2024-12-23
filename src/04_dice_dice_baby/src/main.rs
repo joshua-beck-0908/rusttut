eabf02a13ff9463fee196ddd6b5c09c3bcc6a104
Joshua <joshua.beck.0908@gmail.com>
2024-12-23 21:42:35 +1030

    Learnt about datatypes.

--- src/04_dice_dice_baby/src/main.rs
+++ src/04_dice_dice_baby/src/main.rs
@@ -3,11 +3,13 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    let dice_roll;
+    let mut dice_roll;
     let maximum_dice_roll = 6;
     let high_roll = 4;
+    let run_again_response = 1;
     
     loop {
+        let mut response = String::new();
         dice_roll = rand::rng()
             .random_range(1..=maximum_dice_roll);
         println!("You rolled: {dice_roll}");
@@ -20,5 +22,27 @@ fn main() {
             Ordering::Less => println!("Eh. Good enough. 😐"),
         };
         
+        println!("Select an option.\n(0) roll again\n(1) Finish.");
+        io::stdin()
+            .read_line(&mut response)
+            .expect("Error reading line.");
+        
+        let response: u32 = match response
+            .trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("This is not a number. Please proceed to feel shame.");
+                    println!("Here's another NUMBER while you wait.");
+                    continue;
+                }
+            };
+
+        match response.cmp(&run_again_response) {
+            Ordering::Greater => println!("Invalid response. You'll get another number now.😑"),
+            Ordering::Equal => break,
+            Ordering::Less => println!("Okay, coming right up!😁"),
+        }
     }
+    println!("Done!");
 }
---
+++ src/05_your_type/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let _the_truth: bool = true;
+    let _a_lie = false;
+    let _bite_me: u8 = 40;
+    let _can_i_have_a_word: u16 = 2000;
+    let _equal_opposite: i16 = -2000;
+    let _protection_racket: u32 = 4000000;
+    let _long_long_name: i64 = 123_456_789_012;
+    let _a_hex_on_you: u32 = 0xDEADBEEF;
+    let _binary_decision = 0b0001;
+    let _bees_bees_bees = 0o712;
+    let _character_reference = '🤠';
+    let _single_life = 1.2f32;
+    let _double_trouble = 5e12;
+    let mut _mutant = 5;
+    const _MADE_UP_MY_MIND: u8 = 0x10;
+    let _stringed_instrument = "violin";
+    let _anything_goes =  ("frog", 'a', 200, -2.4);
+    let _shopping_list = ["bread", "eggs", "cheese"];
+    let _99_problems = ["problem"; 99];
+}

5fdbffde451fd01a62d59bfed6f2bbc0f87d1462
Joshua <joshua.beck.0908@gmail.com>
2025-03-10 19:34:50 +1030

    Learnt about HashMaps.

--- src/12_collectors_edition/src/main.rs
+++ src/12_collectors_edition/src/main.rs
@@ -1,21 +1,60 @@
+use std::collections::HashMap;
+
+// Yes... I know, floating your currency is dangerous.
 fn main() {
-    // Yes... I know, floating your currency is dangerous.
-    let mut prices = vec![22.5, 29.1, 0.9, 100.25];
+    let discount_25_percent = vec!["Coats", "Hats", "Sunglasses"];
+    let discount_40_percent = vec!["Ribbons", "Lipstick"];
+    // Apparently you don't need to specify the type here because it can be
+    // statically determined. The IDE still shows the implied type.
+    let mut discounted_items = HashMap::new();
+    let mut pitches = Vec::new();
+
+    for item in discount_25_percent {
+        add_discounted_item(&mut discounted_items, item, 25.0);
+    }
     
-    println!("Original prices: {:?}", prices);
-    save_twenty_five_percent(&mut prices);
-    println!("Sale prices: {:?}", prices);
-    println!("Fourth item is now: ${}", prices[3]);
-    println!("No rainchecks. While stocks last. Terms and conditions apply. See in store for details.");
+    for item in discount_40_percent {
+        add_discounted_item(&mut discounted_items, item, 40.0);
+    }
+
+    pitches.push(get_discount_spiel(&discounted_items, "Coats", 22.50));
+    pitches.push(get_discount_spiel(&discounted_items, "Ribbons", 12.30));
+    pitches.push(get_discount_spiel(&discounted_items, "Chocolates", 4.55));
+    
+    for pitch in pitches {
+        println!("{pitch}");
+    }
+
 }
 
-// This originally caused some confusion when I had:
-// fn save_twenty_five_percent(mut items: &Vec<f64> ) {
-// This apparently means something very different.
-// The thing being *pointed to* can change, but not the contents.
-// See: https://stackoverflow.com/questions/28587698/whats-the-difference-between-placing-mut-before-a-variable-name-and-after-the
-fn save_twenty_five_percent(items: &mut Vec<f64> ) {
-    for item in items {
-        *item *= 0.75;
+// This was surprisingly confusing to get right....
+// I had &str in the HashMap since I only intended to use quoted strings.
+// But it kept on complaining about lifetimes, so I switched it to String.
+// I'm still learning, don't judge me!
+fn add_discounted_item(
+    discount_list: &mut HashMap<String, f64>,
+    category_name: &str,
+    discount_percent: f64) {
+    
+    discount_list.insert(category_name.to_string(), discount_percent);
+}
+
+// TODO: Grammar...
+fn get_discount_spiel(
+    discount_list: &HashMap<String, f64>, 
+    category_name: &str,
+    original_price: f64) -> String {
+    let discount = discount_list.get(category_name).copied().unwrap_or(0.0);
+    
+    let new_price = original_price * (discount / 100.0);
+
+    if discount > 30.0 {
+        return format!("This {category_name} was {original_price}, now only {new_price}. What an incredible bargin!");
+    } else if discount > 0.0 {
+        return format!("This {category_name} was {original_price}, but is now only {new_price}. What a deal!");
+    } else {
+        return format!("No sales on {category_name}, buy it now at {original_price} or check back next week!");
     }
+
+
 }
